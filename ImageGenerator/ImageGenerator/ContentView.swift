6647a9ad322727680583eba03f48fe6e42091088
Donguk Kim <ehddnr73@naver.com>
2023-02-24 14:07:38 +0900

    UI Contents 구성

--- ImageGenerator/ImageGenerator/ContentView.swift
+++ ImageGenerator/ImageGenerator/ContentView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var viewModel = ViewModel()
+    @State var text = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+            } else {
+                Text("Type prompt to generate image!")
+            }
+            Spacer()
+            TextField("Type prompt here...", text: $text)
+                .padding()
+            Button("Generate") {
+                Task {
+                    if !text.trimmingCharacters(in: .whitespaces).isEmpty {
+                        await viewModel.generateImage(prompt: text)
+                    }
+                }
+                text = ""
+            }
+        }
+        .navigationTitle("Image Generator")
+        .onAppear {
+            viewModel.setUp()
         }
         .padding()
     }
