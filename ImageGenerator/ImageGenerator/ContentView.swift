2068c44620ccbba0709adfa60cdd88b64686f9ce
Donguk Kim <ehddnr73@naver.com>
2023-02-24 15:21:44 +0900

    UI Contents 구성
    - NavigationView embed
    - image 바인딩

--- ImageGenerator/ImageGenerator/ContentView.swift
+++ ImageGenerator/ImageGenerator/ContentView.swift
@@ -12,33 +12,36 @@ struct ContentView: View {
     @State var text = ""
     
     var body: some View {
-        VStack {
-            if let image = viewModel.image {
-                Image(uiImage: image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .scaledToFit()
-                    .frame(width: 250, height: 250)
-            } else {
-                Text("Type prompt to generate image!")
-            }
-            Spacer()
-            TextField("Type prompt here...", text: $text)
-                .padding()
-            Button("Generate") {
-                Task {
-                    if !text.trimmingCharacters(in: .whitespaces).isEmpty {
-                        await viewModel.generateImage(prompt: text)
+        NavigationView {
+            VStack {
+                Spacer()
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .scaledToFit()
+                        .frame(width: 250, height: 250)
+                } else {
+                    Text("Type prompt to generate image!")
+                }
+                Spacer()
+                TextField("Type prompt here...", text: $text)
+                    .padding()
+                Button("Generate") {
+                    Task {
+                        if !text.trimmingCharacters(in: .whitespaces).isEmpty {
+                            await viewModel.generateImage(prompt: text)
+                            text = ""
+                        }
                     }
                 }
-                text = ""
             }
+            .navigationTitle("Image Generator")
+            .onAppear {
+                viewModel.setUp()
+            }
+            .padding()
         }
-        .navigationTitle("Image Generator")
-        .onAppear {
-            viewModel.setUp()
-        }
-        .padding()
     }
 }
 
