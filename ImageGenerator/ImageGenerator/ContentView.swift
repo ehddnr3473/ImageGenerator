edae2fa41f5c5c8d8f2fa511af2384ac521d1741
Donguk Kim <ehddnr73@naver.com>
2023-02-24 15:54:54 +0900

    UI 구성
    - ProgressView 생성
    - 이미지 생성 중 다른 작업을 추가할 수 없도록 개선
    - 그 외 배치 수정

--- ImageGenerator/ImageGenerator/ContentView.swift
+++ ImageGenerator/ImageGenerator/ContentView.swift
@@ -10,37 +10,55 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject private var viewModel = ViewModel()
     @State var text = ""
+    @State var isGenerating = false
     
     var body: some View {
         NavigationView {
-            VStack {
-                Spacer()
-                if let image = viewModel.image {
-                    Image(uiImage: image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .scaledToFit()
-                        .frame(width: 250, height: 250)
-                } else {
-                    Text("Type prompt to generate image!")
-                }
-                Spacer()
-                TextField("Type prompt here...", text: $text)
-                    .padding()
-                Button("Generate") {
-                    Task {
-                        if !text.trimmingCharacters(in: .whitespaces).isEmpty {
-                            await viewModel.generateImage(prompt: text)
-                            text = ""
+            ZStack {
+                VStack {
+                    Spacer()
+                    
+                    if let image = viewModel.image {
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 250, height: 250)
+                    } else {
+                        Text("Type prompt to generate image!")
+                    }
+                    
+                    Spacer()
+                    
+                    TextField("Type prompt here...", text: $text)
+                        .padding()
+                        .foregroundColor(.black)
+                    
+                    Button("Generate") {
+                        Task {
+                            if !text.trimmingCharacters(in: .whitespaces).isEmpty {
+                                isGenerating = true
+                                await viewModel.generateImage(prompt: text)
+                                text = ""
+                                isGenerating = false
+                            }
                         }
                     }
+                    .disabled(isGenerating)
+                }
+                .navigationTitle("Image Generator")
+                .foregroundColor(.pink)
+                .onAppear {
+                    viewModel.setUp()
+                }
+                .padding()
+                
+                if isGenerating {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .tint(.pink)
+                        .scaleEffect(2)
                 }
             }
-            .navigationTitle("Image Generator")
-            .onAppear {
-                viewModel.setUp()
-            }
-            .padding()
         }
     }
 }
