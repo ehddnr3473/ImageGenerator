e6ee6c8bc79b23b69b99119837ffe03e457cd46e
Donguk Kim <ehddnr73@naver.com>
2023-02-24 12:00:45 +0900

    API를 호출해서 이미지를 생성하는 메서드 정의

--- ImageGenerator/ImageGenerator/ViewModel.swift
+++ ImageGenerator/ImageGenerator/ViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import OpenAIKit
+import UIKit
 
 final class ViewModel: ObservableObject {
     @frozen enum Constants {
@@ -20,5 +21,24 @@ final class ViewModel: ObservableObject {
             organization: "Personal",
             apiKey: Constants.key
         ))
+    }
+    
+    func generateImage(prompt: String) async -> UIImage? {
+        guard let openAI = openAI else { return nil }
+        
+        do {
+            let params = ImageParameters(
+                prompt: prompt,
+                resolution: .medium,
+                responseFormat: .base64Json
+            )
+            let result = try await openAI.createImage(parameters: params)
+            let imageData = result.data[0].image
+            let image = try openAI.decodeBase64Image(imageData)
+            return image
+        } catch {
+            print(String(describing: error))
+            return nil
+        }
     }
 }
