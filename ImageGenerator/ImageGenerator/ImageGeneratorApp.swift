7b953b4dbe617f6eacaa2848abc596aa3158d350
Donguk Kim <ehddnr73@naver.com>
2023-02-24 11:33:30 +0900

    ImageGenerator SwiftUI 프로젝트 생성

---
+++ ImageGenerator/ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by 김동욱 on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
